6c40de464e5cc6890645890b81747ac022806f9c
AtomHeartFather <quito@mail.ru>
2022-02-13 17:05:54 +0300

    перед пробой SAGA

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -10,15 +10,13 @@ export function createPost(post) {
 
 export function fetchPosts() {
 	return async dispatch => {
-		// const response = await fetch('http://z.bokus.ru/test.json', {mode: 'no-cors'})
-		const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
-			// headers: {
-   //      		'content-type': 'application/json;charset=utf-8',
-   //      		'Access-Control-Allow-Origin': '*',
-   //      		'Access-Control-Allow-Methods': '*',
-   //      		'Access-Control-Allow-Headers': '*'
-   //      	}
-		
+
+		const proxyUrl = 'https://cors.eu.org/'
+		const targetUrl = 'http://z.bokus.ru/test.json'
+
+		const response = await fetch(proxyUrl + targetUrl)
+		// const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')  //рабочий вариант
+
 		//console.log(response.json())
 
 		const json = await response.json()
